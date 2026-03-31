72371cba9d8c57c92a967809f522fe74f3b2e9a4
Nawat Ruengdechawiwat <nawat0035@gmail.com>
2026-03-31 14:31:28 +0700

    "lab13_1complete"

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -4,6 +4,32 @@
 using namespace std;
 
 void stat(const double[],int,double[]);
+void stat(const double A[], int N, double B[]) {
+    int i;
+    double sum = 0;
+    double sum2 = 0;
+    double product = 1;
+    double harmosum = 0;
+    double max = A[0];
+    double min = A[0];
+
+    for(i = 0; i < N; i++) {
+        sum = sum + A[i];
+        sum2 = sum2 + A[i] * A[i];
+        product = product * A[i];
+        harmosum = harmosum + 1 / A[i];
+        if(A[i] > max) max = A[i];
+        if(A[i] < min) min = A[i];
+    }
+    double mean = sum / N;
+
+    B[0] = mean;                                  
+    B[1] = sqrt (sum2 / N - mean * mean);          
+    B[2] = pow(product, 1.0 / N);                 
+    B[3] = N / harmosum;                             
+    B[4] = max;                                    
+    B[5] = min;                                  
+}
 
 int main(){
     double A[] = {1.2,3.5,6.9,7.8,12.5,0.5};
